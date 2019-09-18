e6714e5cc6f0560750bbbc85753120db174c0c2f
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2019-09-18 14:50:34 +0530

    Create api.py

---
+++ EE/api.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!. This is Flask Test Part 1"
+
+if __name__ == "__main__":
+    job_id = 1234
+    app.run(host= '0.0.0.0')
